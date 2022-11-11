ee6bb9d71cebc9eae60334679ca24094b86348d1
Ivelina Dimitrova <dimitrovaivi@gmail.com>
2022-11-11 14:42:10 +0200

    Added extended logic for adding/updating CinemaTicket

--- MovieCinema/MovieStore.BL/CommandHandler/CinemaTicketsCommandHandler/MakeCinemaTicketCommandHandler.cs
+++ MovieCinema/MovieStore.BL/CommandHandler/CinemaTicketsCommandHandler/MakeCinemaTicketCommandHandler.cs
@@ -13,21 +13,45 @@ namespace MovieStore.BL.CommandHandler.CinemaTicketsCommandHandler
     {
         private readonly ICinemaTicketRepository _cinemaTicketRepository;
         private readonly ITicketsReportRepository _ticketsReportRepository;
+        private readonly IMovieRepository _movieRepository;
         private ILogger<MakeCinemaTicketCommandHandler> _logger;
         private readonly IMapper _mapper;
 
-        public MakeCinemaTicketCommandHandler(ICinemaTicketRepository cinemaTicketRepository, ILogger<MakeCinemaTicketCommandHandler> logger, IMapper mapper, ITicketsReportRepository ticketsReportRepository)
+        public MakeCinemaTicketCommandHandler(ICinemaTicketRepository cinemaTicketRepository, ILogger<MakeCinemaTicketCommandHandler> logger, IMapper mapper, ITicketsReportRepository ticketsReportRepository, IMovieRepository movieRepository)
         {
             _cinemaTicketRepository = cinemaTicketRepository;
             _logger = logger;
             _mapper = mapper;
             _ticketsReportRepository = ticketsReportRepository;
+            _movieRepository = movieRepository;
         }
 
         public async Task<GetCinemaTicketResponse> Handle(MakeCinemaTicketCommand request, CancellationToken cancellationToken)
         {
             try
             {
+                var moviesExistCount = 0;
+
+                foreach (var movie in request.ticket.Movies)
+                {
+                    var exists = _movieRepository.GetMovieById(movie.Id).Result;
+
+                    if(exists != null)
+                    {
+                        moviesExistCount++;
+                    }
+                }
+
+                if(moviesExistCount != request.ticket.Movies.Count())
+                {
+                    return new GetCinemaTicketResponse()
+                    {
+                        HttpStatusCode = HttpStatusCode.BadRequest,
+                        Message = "Cinema ticket cannot be updated because this/these movie/s doesn't exist..",
+                        CinemaTicket = null
+                    };
+                }
+
                 var ticketMapper = _mapper.Map<CinemaTicket>(request.ticket);
 
                 ticketMapper.LastUpdated = DateTime.Now;
--- MovieCinema/MovieStore.BL/CommandHandler/CinemaTicketsCommandHandler/UpdateCinemaTicketCommandHandler.cs
+++ MovieCinema/MovieStore.BL/CommandHandler/CinemaTicketsCommandHandler/UpdateCinemaTicketCommandHandler.cs
@@ -13,15 +13,17 @@ namespace MovieStore.BL.CommandHandler.CinemaTicketsCommandHandler
     {
         private readonly ICinemaTicketRepository _cinemaTicketRepository;
         private readonly ITicketsReportRepository _ticketsReportRepository;
+        private readonly IMovieRepository _movieRepository;
         private ILogger<UpdateCinemaTicketCommandHandler> _logger;
         private readonly IMapper _mapper;
 
-        public UpdateCinemaTicketCommandHandler(ICinemaTicketRepository cinemaTicketRepository, ILogger<UpdateCinemaTicketCommandHandler> logger, IMapper mapper, ITicketsReportRepository ticketsReportRepository)
+        public UpdateCinemaTicketCommandHandler(ICinemaTicketRepository cinemaTicketRepository, ILogger<UpdateCinemaTicketCommandHandler> logger, IMapper mapper, ITicketsReportRepository ticketsReportRepository, IMovieRepository movieRepository)
         {
             _cinemaTicketRepository = cinemaTicketRepository;
             _logger = logger;
             _mapper = mapper;
             _ticketsReportRepository = ticketsReportRepository;
+            _movieRepository = movieRepository;
         }
 
         public async Task<GetCinemaTicketResponse> Handle(UpdateCinemaTicketCommand request, CancellationToken cancellationToken)
@@ -37,6 +39,28 @@ namespace MovieStore.BL.CommandHandler.CinemaTicketsCommandHandler
                         Message = "The cinema ticket with this Id doesn't exists!",
                         CinemaTicket = null
                     };
+
+                var moviesExistCount = 0;
+
+                foreach (var movie in request.ticket.Movies)
+                {
+                    var exists = _movieRepository.GetMovieById(movie.Id).Result;
+
+                    if (exists != null)
+                    {
+                        moviesExistCount++;
+                    }
+                }
+
+                if (moviesExistCount != request.ticket.Movies.Count())
+                {
+                    return new GetCinemaTicketResponse()
+                    {
+                        HttpStatusCode = HttpStatusCode.BadRequest,
+                        Message = "Cinema ticket cannot be updated because this/these movie/s doesn't exist..",
+                        CinemaTicket = null
+                    };
+                }
 
                 var ticketMapper = _mapper.Map<CinemaTicket>(request.ticket);
 
